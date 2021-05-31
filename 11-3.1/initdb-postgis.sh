4fc7cc7ecf10baed7351a2ee7bfbb946cf3e39a2
Francisco Lopez <francisco-9708@hotmail.es>
2021-05-31 01:23:15 -0500

    Update initdb-postgis.sh

--- 11-3.1/initdb-postgis.sh
+++ 11-3.1/initdb-postgis.sh
@@ -18,5 +18,7 @@ for DB in template_postgis "$POSTGRES_DB"; do
 		CREATE EXTENSION IF NOT EXISTS postgis_topology;
 		CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
 		CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;
+		CREATE EXTENSION IF NOT EXISTS intarray;
+		CREATE EXTENSION IF NOT EXISTS unaccent;
 EOSQL
 done
